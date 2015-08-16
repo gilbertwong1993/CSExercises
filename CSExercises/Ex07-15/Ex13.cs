66e91f01cceaeca1734bf9a86165ccf316f35117
gilbertwong1993 <3flash.03@gmail.com>
2015-08-17 00:53:59 +0800

    Gilbert Wong

--- CSExercises/Ex07-15/Ex13.cs
+++ CSExercises/Ex07-15/Ex13.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/*In the above problem,if you have always to round upwards
+ -how would you do that?*/
+//Program written by Gilbert Wong
 namespace CSExercises
 {
     public class Ex13
     {
         public static void Main(string[] args)
         {
-            
+            Console.WriteLine("Enter the distance");
+            double distance = Convert.ToDouble(Console.ReadLine());
+            double totalfare = 2.40 + 0.4 * distance;
+            double fare = Math.Ceiling(totalfare, 1);
+            Console.WriteLine("The fare is ${0:0.00}.", fare);
         }
     }
 }
