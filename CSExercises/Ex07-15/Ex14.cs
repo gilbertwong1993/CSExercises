3f5bc06f309d7321a1b86385311c698b0a46cfbe
gilbertwong1993 <3flash.03@gmail.com>
2015-08-17 00:51:03 +0800

    Gilbert Wong

--- CSExercises/Ex07-15/Ex14.cs
+++ CSExercises/Ex07-15/Ex14.cs
@@ -1,10 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*Consider the simple Geometric example of determining the area of a triangle,given the lengths of its three sides a,b and c.
+ Use the formula: AREA = SQUAREROOT[s(s-a)(s-b)(s-c)]
+ where s = (a+b+c)/2
+ Does your program always work? If not alter it to consider situations fro which it fails.*/
+//Program writtem by Gilbert Wong
 namespace CSExercises
 {
     public class Ex14
     {
         public static void Main(string[] args)
         {
-            
+            Console.WriteLine("Enter the three sides of a triangle");
+            var a = Convert.ToDouble(Console.ReadLine());
+            var b = Convert.ToDouble(Console.ReadLine());
+            var c = Convert.ToDouble(Console.ReadLine());
+            double s = (a + b + c) * 0.5;
+            double AREA = Math.Sqrt(s*(s-a)*(s-b)*(s-c));
+            Console.WriteLine("The area of this triangle is {0}.",AREA);
         }
     }
 }
--- CSExercises/Ex07-15/Ex15.cs
+++ CSExercises/Ex07-15/Ex15.cs
@@ -1,10 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*Given a three-digit integer as input write a C# program to determine whether the number is an Armstrong number.
+ An Armstrong number is one for which the sum of each digit raised to the power of number of digits results in the number itself.
+ For a three digit number 153 = 1^3 + 5^3 + 3^3
+ Confine the input to 3 digit examples only ie.,number values 100...999.*/
+//Program written by Gilbert Wong
 namespace CSExercises
 {
     public class Ex15
     {
         public static void Main(string[] args)
         {
-            
+            Console.WriteLine("Enter a 3 digit number");
         }
     }
 }
