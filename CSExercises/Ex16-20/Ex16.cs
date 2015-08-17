4df2463260b429723b99c08c29141e55ca85105d
gilbertwong1993 <3flash.03@gmail.com>
2015-08-17 12:01:34 +0800

    gil

--- CSExercises/Ex16-20/Ex16.cs
+++ CSExercises/Ex16-20/Ex16.cs
@@ -1,10 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*Write a program that would request for your name and gender(M or F assume capital letter)and print a greeting like this:
+ Good Morning Mr.Venkat
+ where you entered Venkat for name and M for gender,use Mr. for males and Ms. for females.*/
+//Program written by Gilbert Wong
 namespace CSExercises
 {
     public class Ex16
     {
         public static void Main(string[] args)
         {
-            
+            Console.WriteLine("What's your name?");
+            string YourName = Console.ReadLine();
+            Console.WriteLine("What's your gender?(M or F assume capital letter)");
+            string Gender = Console.ReadLine();
+            if (Gender == "M")
+                Console.WriteLine("Good Morning Mr." + YourName);
+            if (Gender == "F")
+                Console.WriteLine("Good Morning Mrs." + YourName);
+            else 
+                Console.WriteLine("Please enter your gender correctly.");
         }
     }
 }
--- CSExercises/Ex16-20/Ex17.cs
+++ CSExercises/Ex16-20/Ex17.cs
@@ -1,10 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*Write a program that would request for your name,gender and age and would print a greeting like this:
+ Good Morning Uncle Sam(if Sam,M,45 is entered)
+(any gentlemen 40 years or more is Uncle any lady 40 years or more is Aunty,if less than forty they become just Mr or Ms. as the case may be.)*/
+//Program written by Gilbert Wong
 namespace CSExercises
 {
     public class Ex17
     {
         public static void Main(string[] args)
         {
-            
+            Console.WriteLine("What's your name?");
+            string YourName = Console.ReadLine();
+            Console.WriteLine("What's your gender?(M or F assume capital letter)");
+            string Gender = Console.ReadLine();
+            Console.WriteLine("How old are you?");
+            int Age = Convert.ToInt32(Console.ReadLine());
+            if ((Gender == "M") && (Age >= 40))
+                Console.WriteLine("Good Morning Uncle " + YourName);
+            if ((Gender == "M") && (Age < 40) && (Age >= 0))
+                Console.WriteLine("Good Morning Mr." + YourName);
+            if ((Gender == "F") && (Age >= 40))
+                Console.WriteLine("Good Morning Aunty " + YourName);
+            if ((Gender == "F") && (Age < 40) && (Age >= 0))
+                Console.WriteLine("Good Morning Mrs." + YourName);
+            else if ((Gender != "M") && (Gender != "F"))
+                Console.WriteLine("Please enter your gender correctly.");
+            else if (Age < 0)
+                Console.WriteLine("Please enter your age correctly.");
         }
     }
 }
